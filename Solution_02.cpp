958a39d5acee0d2aa5eef03c9266237c6997b34f
BruceCheen <36027814+BruceCheen@users.noreply.github.com>
2019-08-22 09:02:52 +0800

    Update Solution_02.cpp

--- Solution_02.cpp
+++ Solution_02.cpp
@@ -1,73 +1,47 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 class Solution {
 public:
-    bool Find(int target, vector<vector<int> > array) {
-		int rows = array.size();
-		int cols = array[0].size();
-        if (rows==0 || cols==0)
+	void replaceSpace(char *str, int length) {
+		if (str == NULL || length <= 0) return;
+		int ori_length = 0;//字符串原来长度
+		int new_length = 0;//字符串新长度
+		int num_blank = 0;//空字符串的个数
+		while (str[ori_length] != '\0')
 		{
-			cout<<"array.sise()==0 or array[0].size()==0"<<endl;
-			return false;
+			if (str[ori_length] == ' ')
+			{
+				num_blank++;
+			}
+			ori_length++;
 		}
-		if (array[0][0] > target || array[rows-1][cols-1] < target)
-			return false;
-		int r=0; int c=cols-1;
-
-		while(array[r][c] != target && c>=0 && r<=rows-1)
+		if (num_blank == 0) return;
+		new_length = ori_length + 2 * num_blank;
+		//必须要将末尾的'\0'也要赋值
+		int last = new_length;
+		int pre = ori_length;
+		while (pre < last && pre >= 0)
 		{
-			if(array[r][c] == target) return true;
-			if(array[r][c] < target) r++;
-			if(array[r][c] > target) c--;
-
+			if (str[pre] != ' ')
+			{
+				str[last--] = str[pre];
+			}
+			else {
+				str[last--] = '0';
+				str[last--] = '2';
+				str[last--] = '%';
+			}
+			pre--;
 		}
-		return false;
-    }
+
+	}
 };
 
 int main()
 {
-	vector<vector<int> > array;
-	vector<int> vec1;
-	for (int i=1; i<6; i++)
-	{
-		vec1.push_back(i);
-	}
-	//#######################
-	vector<int> vec2;
-	for (int i=2; i<7; i++)
-	{
-		vec2.push_back(i);
-	}
-	//#######################
-	vector<int> vec3;
-	for (int i=3; i<8; i++)
-	{
-		vec3.push_back(i);
-	}
-	//#######################
-	vector<int> vec4;
-	for (int i=4; i<9; i++)
-	{
-		vec4.push_back(i);
-	}
-	//#######################
-	vector<int> vec5;
-	for (int i=5; i<10; i++)
-	{
-		vec5.push_back(i);
-	}
-	array.push_back(vec1);
-	array.push_back(vec2);
-	array.push_back(vec3);
-	array.push_back(vec4);
-	array.push_back(vec5);
 	Solution slove;
-
-	if(slove.Find(8, array))
-	{
-		cout<<"Finded!"<<endl;
-	}
+	char str[15] = " helloworld";
+	slove.replaceSpace(str, 100);
+	
 	return 0;
 }
