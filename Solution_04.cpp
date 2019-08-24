996b2e7d211e9d45505d2dcc17667c5424d8b292
BruceCheen <36027814+BruceCheen@users.noreply.github.com>
2019-08-24 09:41:53 +0800

    Update Solution_04.cpp

--- Solution_04.cpp
+++ Solution_04.cpp
@@ -1,73 +1,58 @@
+/*
+	题目描述：
+	输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。
+	假设输入的前序遍历和中序遍历的结果中都不含重复的数字。例如
+	输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，
+	则重建二叉树并返回。
+ */
+
 #include <iostream>
 #include <vector>
 using namespace std;
+
+
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ };
+
+
 class Solution {
 public:
-	bool Find(int target, vector<vector<int> > array) {
-		int rows = array.size();
-		int cols = array[0].size();
-		if (rows == 0 || cols == 0)
-		{
-			cout << "array.sise()==0 or array[0].size()==0" << endl;
-			return false;
-		}
-		if (array[0][0] > target || array[rows - 1][cols - 1] < target)
-			return false;
-		int r = 0; int c = cols - 1;
+	TreeNode* reConstructBinaryTree(vector<int> pre, vector<int> vin) {
+		if (pre.size() != vin.size()) return NULL;
+		int num_nodes = pre.size();
+		TreeNode* root = reConstructBinaryTree(pre, 0, num_nodes - 1, vin, 0, num_nodes - 1);
+		return root;
+	}
+	TreeNode* reConstructBinaryTree(const vector<int> &pre, int pre_start, int pre_end, const vector<int> &vin, int vin_start, int vin_end)
+	{
+		if (pre_start > pre_end || vin_start > vin_end) return NULL;
+		TreeNode* root = new TreeNode(pre[pre_start]);
 
-		while (array[r][c] != target && c >= 0 && r <= rows - 1)
+		for(int i= vin_start; i<=vin_end; i++)
 		{
-			if (array[r][c] == target) return true;
-			if (array[r][c] < target) r++;
-			if (array[r][c] > target) c--;
-
+			//在中序序列中寻找先序序列中的头节点
+			if(vin[i] == root->val)
+			{
+				root->left = reConstructBinaryTree(pre, pre_start + 1, pre_start + i - vin_start, vin, vin_start, i - 1);
+				root->right = reConstructBinaryTree(pre, pre_start + i - vin_start + 1, pre_end, vin, i + 1, vin_end);
+			}
 		}
-		return false;
+		return root;
 	}
 };
 
 int main()
 {
-	vector<vector<int> > array;
-	vector<int> vec1;
-	for (int i = 1; i < 6; i++)
-	{
-		vec1.push_back(i);
-	}
-	//#######################
-	vector<int> vec2;
-	for (int i = 2; i < 7; i++)
-	{
-		vec2.push_back(i);
-	}
-	//#######################
-	vector<int> vec3;
-	for (int i = 3; i < 8; i++)
-	{
-		vec3.push_back(i);
-	}
-	//#######################
-	vector<int> vec4;
-	for (int i = 4; i < 9; i++)
-	{
-		vec4.push_back(i);
-	}
-	//#######################
-	vector<int> vec5;
-	for (int i = 5; i < 10; i++)
-	{
-		vec5.push_back(i);
-	}
-	array.push_back(vec1);
-	array.push_back(vec2);
-	array.push_back(vec3);
-	array.push_back(vec4);
-	array.push_back(vec5);
-	Solution slove;
+	Solution solve;
 
-	if (slove.Find(8, array))
-	{
-		cout << "Finded!" << endl;
-	}
-	return 0;
+	vector<int> pre = { 1, 2, 4, 7, 3, 5, 6, 8 };
+	vector<int> vin = { 4, 7, 2, 1, 5, 3, 8, 6 };
+	TreeNode* root = solve.reConstructBinaryTree(pre, vin);
+	cout << "end!";
 }
+
+
